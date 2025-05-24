0c1e6da33f61a3ab1d60d1d8b3a12d2a5a0693b0
gokdenizayrilmis <ayrilmis.gokdeniz@gmail.com>
2025-05-24 15:00:45 +0300

    Proje olusturuldu.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SudokuApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
